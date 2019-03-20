01b511a00c61392fbbfa23b31814b1804fb67406
Do Trung Kien <trungkien.keio@gmail.com>
2019-03-20 09:37:40 +0700

    complete game logic

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -39,131 +39,255 @@ public class GridManager : Singleton<GridManager>
     public void UpdateStatus()
     {
         int newScore = 0;
+        int dim = 9;
+
         // Columns
-        for (int i = 0; i < 9; i++)
+        for (int i = 0; i < dim; i++)
         {
-            int j = 0;
-            while (j < 8)
+            int[] tempList = new int[dim];
+            Dictionary<int, int[]> map = new Dictionary<int, int[]>();
+            for (int j = 0; j < dim; j++)
             {
-                if (grid[i, j] == 0 || grid[i, j] != grid[i, j + 1])
-                {
-                    j++;
-                    if (j > 8) break;
-                }
-                else
-                {
-                    int k = j;
-                    while (k < 9 && grid[i, j] == grid[i, k]) k++;
-                    if (k - j >= 3)
-                    {
-                        newScore += grid[i, j] * (k - j);
-                        Vector3 from = GridToPos(new int[2] { i, j });
-                        Vector3 to = GridToPos(new int[2] { i, k - 1 });
-                        DrawLine(from, to);
-                    }
-                    j = k;
-                }
+                tempList[j] = grid[i, j];
+                map[j] = new int[] { i, j };
             }
+            newScore += searchAndMatch(tempList, map);
+            // int j = 0;
+            // while (j < 8)
+            // {
+            //     if (grid[i, j] == 0 || grid[i, j] != grid[i, j + 1])
+            //     {
+            //         j++;
+            //         if (j > 8) break;
+            //     }
+            //     else
+            //     {
+            //         int k = j;
+            //         while (k < 9 && grid[i, j] == grid[i, k]) k++;
+            //         if (k - j >= 3)
+            //         {
+            //             newScore += grid[i, j] * (k - j);
+            //             Vector3 from = GridToPos(new int[2] { i, j });
+            //             Vector3 to = GridToPos(new int[2] { i, k - 1 });
+            //             DrawLine(from, to);
+            //         }
+            //         j = k;
+            //     }
+            // }
         }
         // Rows
-        for (int j = 0; j < 9; j++)
+        for (int j = 0; j < dim; j++)
         {
-            int i = 0;
-            while (i < 8)
+            int[] tempList = new int[dim];
+            Dictionary<int, int[]> map = new Dictionary<int, int[]>();
+            for (int i = 0; i < dim; i++)
             {
-                if (grid[i, j] == 0 || grid[i, j] != grid[i + 1, j])
-                {
-                    i++;
-                    if (i > 8) break;
-                }
-                else
-                {
-                    int k = i + 1;
-                    while (k < 9 && grid[i, j] == grid[k, j]) k++;
-                    if (k - i >= 3)
-                    {
-                        newScore += grid[i, j] * (k - i);
-                        Vector3 from = GridToPos(new int[2] { i, j });
-                        Vector3 to = GridToPos(new int[2] { k - 1, j });
-                        DrawLine(from, to);
-                    }
-                    i = k;
-                }
+                tempList[i] = grid[i, j];
+                map[i] = new int[] { i, j };
             }
+            newScore += searchAndMatch(tempList, map);
+            // int i = 0;
+            // while (i < 8)
+            // {
+            //     if (grid[i, j] == 0 || grid[i, j] != grid[i + 1, j])
+            //     {
+            //         i++;
+            //         if (i > 8) break;
+            //     }
+            //     else
+            //     {
+            //         int k = i + 1;
+            //         while (k < 9 && grid[i, j] == grid[k, j]) k++;
+            //         if (k - i >= 3)
+            //         {
+            //             newScore += grid[i, j] * (k - i);
+            //             Vector3 from = GridToPos(new int[2] { i, j });
+            //             Vector3 to = GridToPos(new int[2] { k - 1, j });
+            //             DrawLine(from, to);
+            //         }
+            //         i = k;
+            //     }
+            // }
         }
-        int dim = 9;
-        for (int k = 2; k < dim * 2 - 2; k++)
+        // lower \ diagonal
+        for (int k = 2; k < dim; k++)
         {
             int[] tempList = new int[k + 1];
+            Dictionary<int, int[]> map = new Dictionary<int, int[]>();
             for (int j = 0; j <= k; j++)
             {
                 int i = k - j;
-                if (i < dim && j < dim)
-                {
-                    tempList[j] = grid[i, j];
-                }
+                tempList[j] = grid[i, j];
+                map[j] = new int[] { i, j };
             }
-
-            int u = 0;
-            while (u < k)
-            {
-                if (tempList[u] == 0 || tempList[u] != tempList[u + 1])
-                {
-                    u++;
-                    if (u > k) break;
-                }
-                else
-                {
-                    int v = u;
-                    while (v < k + 1 && tempList[u] == tempList[v]) v++;
-                    if (v - u >= 3)
-                    {
-                        newScore += tempList[u] * (v - u);
-                        Vector3 from = GridToPos(new int[2] { k - u, u });
-                        Vector3 to = GridToPos(new int[2] { k - v + 1, v - 1 });
-                        DrawLine(from, to);
-                    }
-                    u = v;
-                }
-            }
+            newScore += searchAndMatch(tempList, map);
+            // int u = 0;
+            // while (u < k)
+            // {
+            //     if (tempList[u] == 0 || tempList[u] != tempList[u + 1])
+            //     {
+            //         u++;
+            //         if (u > k) break;
+            //     }
+            //     else
+            //     {
+            //         int v = u;
+            //         while (v < k + 1 && tempList[u] == tempList[v]) v++;
+            //         if (v - u >= 3)
+            //         {
+            //             newScore += tempList[u] * (v - u);
+            //             Vector3 from = GridToPos(map[u]);
+            //             Vector3 to = GridToPos(map[v - 1]);
+            //             DrawLine(from, to);
+            //         }
+            //         u = v;
+            //     }
+            // }
         }
-        for (int k = 2; k < dim * 2 - 2; k++)
+        // upper \ diagonal
+        for (int k = dim - 2; k >= 2; k--)
         {
             int[] tempList = new int[k + 1];
+            Dictionary<int, int[]> map = new Dictionary<int, int[]>();
+
+            for (int j = 0; j <= k; j++)
+            {
+                int i = k - j;
+                tempList[j] = grid[dim - j - 1, dim - i - 1];
+                map[j] = new int[] { dim - j - 1, dim - i - 1 };
+            }
+            newScore += searchAndMatch(tempList, map);
+
+            // int u = 0;
+            // while (u < k)
+            // {
+            //     if (tempList[u] == 0 || tempList[u] != tempList[u + 1])
+            //     {
+            //         u++;
+            //         if (u > k) break;
+            //     }
+            //     else
+            //     {
+            //         int v = u;
+            //         while (v < k + 1 && tempList[u] == tempList[v]) v++;
+            //         if (v - u >= 3)
+            //         {
+            //             newScore += tempList[u] * (v - u);
+            //             Vector3 from = GridToPos(map[u]);
+            //             Vector3 to = GridToPos(map[v - 1]);
+            //             DrawLine(from, to);
+            //         }
+            //         u = v;
+            //     }
+            // }
+        }
+
+        // lower / diagonal
+        for (int k = dim - 2; k >= 2; k--)
+        {
+            int[] tempList = new int[k + 1];
+            Dictionary<int, int[]> map = new Dictionary<int, int[]>();
+
+            for (int j = 0; j <= k; j++)
+            {
+                int i = k - j;
+                tempList[j] = grid[dim - i - 1, dim - j - 1];
+                map[j] = new int[] { dim - i - 1, dim - j - 1 };
+            }
+            newScore += searchAndMatch(tempList, map);
+
+            // Debug.Log(string.Join(" ", tempList));
+
+            // int u = 0;
+            // while (u < k)
+            // {
+            //     if (tempList[u] == 0 || tempList[u] != tempList[u + 1])
+            //     {
+            //         u++;
+            //         if (u > k) break;
+            //     }
+            //     else
+            //     {
+            //         int v = u;
+            //         while (v < k + 1 && tempList[u] == tempList[v]) v++;
+            //         if (v - u >= 3)
+            //         {
+            //             newScore += tempList[u] * (v - u);
+            //             Vector3 from = GridToPos(map[u]);
+            //             Vector3 to = GridToPos(map[v - 1]);
+            //             DrawLine(from, to);
+            //         }
+            //         u = v;
+            //     }
+            // }
+        }
+
+        // upper / diagonal
+        for (int k = 2; k < dim; k++)
+        {
+            int[] tempList = new int[k + 1];
+            Dictionary<int, int[]> map = new Dictionary<int, int[]>();
             for (int i = 0; i <= k; i++)
             {
-                int j = k - i;
-                if (j < dim && i < dim)
-                {
-                    tempList[i] = grid[i, j];
-                }
+                int j = dim - k - 1 + i;
+                tempList[i] = grid[i, j];
+                map[i] = new int[] { i, j };
             }
-
-            int u = 0;
-            while (u < k)
-            {
-                if (tempList[u] == 0 || tempList[u] != tempList[u + 1])
-                {
-                    u++;
-                    if (u > k) break;
-                }
-                else
-                {
-                    int v = u;
-                    while (v < k + 1 && tempList[u] == tempList[v]) v++;
-                    if (v - u >= 3)
-                    {
-                        newScore += tempList[u] * (v - u);
-                        Vector3 from = GridToPos(new int[2] { u, k - u });
-                        Vector3 to = GridToPos(new int[2] { v - 1, k - v + 1 });
-                        DrawLine(from, to);
-                    }
-                    u = v;
-                }
-            }
+            newScore += searchAndMatch(tempList, map);
+            // int u = 0;
+            // while (u < k)
+            // {
+            //     if (tempList[u] == 0 || tempList[u] != tempList[u + 1])
+            //     {
+            //         u++;
+            //         if (u > k) break;
+            //     }
+            //     else
+            //     {
+            //         int v = u;
+            //         while (v < k + 1 && tempList[u] == tempList[v]) v++;
+            //         if (v - u >= 3)
+            //         {
+            //             newScore += tempList[u] * (v - u);
+            //             Vector3 from = GridToPos(map[u]);
+            //             Vector3 to = GridToPos(map[v - 1]);
+            //             DrawLine(from, to);
+            //         }
+            //         u = v;
+            //     }
+            // }
         }
 
         totalScore = newScore;
+    }
+
+    int searchAndMatch(int[] tempList, Dictionary<int, int[]> map)
+    {
+        int u = 0;
+        int len = tempList.Length;
+        int newScore = 0;
+        while (u < len - 1)
+        {
+            if (tempList[u] == 0 || tempList[u] != tempList[u + 1])
+            {
+                u++;
+                if (u > len - 1) break;
+            }
+            else
+            {
+                int v = u;
+                while (v < len && tempList[u] == tempList[v]) v++;
+                if (v - u >= 3)
+                {
+                    newScore += tempList[u] * (v - u);
+                    Vector3 from = GridToPos(map[u]);
+                    Vector3 to = GridToPos(map[v - 1]);
+                    DrawLine(from, to);
+                }
+                u = v;
+            }
+        }
+        return newScore;
     }
 
     void DrawLine(Vector3 from, Vector3 to)
