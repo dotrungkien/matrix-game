6e961dd65a52bf6a649e71497e8958b788cdd2ee
Do Trung Kien <trungkien.keio@gmail.com>
2019-03-22 10:06:45 +0700

    implement create game

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using Phoenix;
 
-public class Connection : MonoBehaviour
+public class Connection : Singleton<Connection>
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector]
+    public string userID;
+
+
+    List<string> currentGames = new List<string>();
+    private Channel channel;
+
+    void Awake()
     {
+        userID = PlayerPrefs.GetString("userid", "");
+        if (userID == "")
+        {
+            Debug.Log("User ID not found. Create a new one.");
+            userID = System.Guid.NewGuid().ToString();
+            PlayerPrefs.SetString("userID", userID);
+        }
+        Debug.Log(string.Format("User ID: {0}", userID));
+
         var socketFactory = new WebsocketSharpFactory();
         var socket = new Socket(socketFactory);
-        Socket.OnOpenDelegate onOpenCallback = () => Debug.Log("Socket connect succesfully!");
-        Socket.OnErrorDelegate onErrorCallback = (e) => Debug.Log("Socket connect failed");
+        Socket.OnOpenDelegate onOpenCallback = () => Debug.Log("Socket connect ok.");
+        Socket.OnErrorDelegate onErrorCallback = (e) => Debug.Log("Socket connect failed.");
         List<string> onMessageData = new List<string>();
         Socket.OnMessageDelegate onMessageCallback = m =>
         {
-            Debug.Log(m);
             onMessageData.Add(m);
         };
 
@@ -24,41 +38,65 @@ public class Connection : MonoBehaviour
         socket.OnError += onErrorCallback;
 
         Dictionary<string, string> socketArgument = new Dictionary<string, string>();
-        socketArgument["id"] = "ahihikienday";
-        socketArgument["vsn"] = "2.0.0";
+        socketArgument["id"] = userID;
 
-        socket.Connect("ws://matrix.heasygame.com/socket", socketArgument);
+        socket.Connect("wss://matrix.heasygame.com/socket", socketArgument);
 
         Message afterJoinMessage = null;
         Message closeMessage = null;
         Message errorMessage = null;
         Message replyMessage = null;
 
-        var roomChannel = socket.MakeChannel("lobby");
-        roomChannel.On(Message.InBoundEvent.phx_error, m =>
+        channel = socket.MakeChannel("lobby");
+        channel.On(Message.InBoundEvent.phx_error, m =>
         {
-            Debug.Log(string.Format("Error message {0}", m));
+            // Debug.Log(string.Format("Error message {0}", m));
             errorMessage = m;
         });
-        roomChannel.On(Message.InBoundEvent.phx_reply, m =>
+        channel.On(Message.InBoundEvent.phx_reply, m =>
         {
-            Debug.Log(string.Format("Reply message {0}", m));
+            // Debug.Log(string.Format("Reply message {0}", m));
             replyMessage = m;
         });
-        roomChannel.On(Message.InBoundEvent.phx_close, m =>
+        channel.On(Message.InBoundEvent.phx_close, m =>
         {
-            Debug.Log(string.Format("Close message {0}", m));
+            // Debug.Log(string.Format("Close message {0}", m));
             closeMessage = m;
         });
-        roomChannel.On("after_join", m =>
+        channel.On("after_join", m =>
         {
-            Debug.Log(string.Format("After join message {0}", m));
+            // Debug.Log(string.Format("After join message {0}", m));
             afterJoinMessage = m;
         });
         var param = new Dictionary<string, object> { };
-        roomChannel.Join(param)
-            .Receive(Reply.Status.Ok, r => Debug.Log(string.Format("Join Channel Succesfully")))
-            .Receive(Reply.Status.Error, r => Debug.Log(string.Format("Join Channel Failed! {0}", r)))
+        channel.Join(param)
+            .Receive(Reply.Status.Ok, r => Debug.Log("Join channel ok."))
+            .Receive(Reply.Status.Error, r => Debug.Log("Join channel failed."))
             .Receive(Reply.Status.Timeout, r => Debug.Log("Time out"));
+    }
+
+    public void CreateNewGame()
+    {
+        var newGame = new Dictionary<string, object>
+        {
+            {"max_player", "2"},
+            {"mode", "easy"},
+            {"time_limit", "0"},
+            {"password", ""}
+        };
+
+        channel.Push("new_game", newGame)
+        .Receive(Reply.Status.Ok, r =>
+        {
+            string gameID = r.response.GetValue("game_id").ToString();
+            currentGames.Add(gameID);
+            Debug.Log(string.Format("Create new game ok. {0}", gameID));
+        })
+        .Receive(Reply.Status.Error, r => Debug.Log("Create new game failed."));
+    }
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.DeleteKey("userid");
     }
 }
---
+++ Assets/Scripts/LobbyUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUI : MonoBehaviour
+{
+    public Text userID;
+    public Button createGameButton;
+
+    void Start()
+    {
+        userID.text = Connection.GetInstance().userID;
+        createGameButton.onClick.AddListener(Connection.GetInstance().CreateNewGame);
+    }
+}
